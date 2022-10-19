c2a9d8586975257eea1de76f3b5f6d325bdeb6ac
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-20 00:01:22 +0530

    view page

---
+++ src/app/viewrecipe/viewrecipe.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewrecipe',
+  templateUrl: './viewrecipe.component.html',
+  styleUrls: ['./viewrecipe.component.css']
+})
+export class ViewrecipeComponent implements OnInit {
+
+  constructor() { }
+
+  recipeData=[]
+
+  ngOnInit(): void {
+  }
+
+}
